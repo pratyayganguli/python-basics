7cce15f6812bff81fbd1bb4925de7d81aefc5278
Partha Sarathi Saha <mepartha.saha@gmail.com>
2022-10-18 16:37:59 +0530

    practice package added

---
+++ practice.py
@@ -0,0 +1,39 @@
+def data():
+    return "print"
+print(data())
+
+def param():
+    print("bayleaf")
+param()
+
+def validtweet(characters):
+    if (characters <= 140):
+        return "Valid Tweet"
+    else:
+        return "Not Valid Tweet"
+
+print(validtweet(150))
+
+def positivevibes(value):
+    if (value >= 0):
+        return "Good Vibes"
+    else:
+        return "Red Flag"
+print(positivevibes(-98))
+
+def bloodpressureH(Hg):
+    if (Hg >= 130):
+        return "High"
+    elif (Hg <120):
+        return "Low"
+    else:
+        return "Normal"
+def bloodpressureL(Hg):
+    if (Hg < 80):
+        return "Low"
+    elif (Hg >85):
+        return "High"
+    else:
+        return "Normal"
+print(bloodpressureH(135))
+print(bloodpressureL(80))
