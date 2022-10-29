d6367fa0ad376b2c8921588b8323720bd55e97f6
Pratyay Ganguli <pratyay.ganguli@ziroh.com>
2022-10-29 11:20:45 +0530

    build fixes

---
+++ Readme.md
@@ -0,0 +1,9 @@
+
+
+
+#### Working with Lists ####
+
+#### Tuples ####
+
+
+
---
+++ src/main/task/electricitybills.py
@@ -0,0 +1,13 @@
+'''
+    @author Pratyay Ganguli
+'''
+
+'''
+    bills = [
+        lineId, customerGender, modeOfPayment 
+    ]
+'''
+
+import random as rd
+
+print(rd.randrange(1, 19))
