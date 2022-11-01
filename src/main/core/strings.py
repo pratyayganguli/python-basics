874cb8e496ae840561c4c8ad303b762e33885e5f
Pratyay Ganguli <pratyay.ganguli@ziroh.com>
2022-11-02 00:02:00 +0530

    Update strings.py
    Added some more method implementations of strings

--- src/main/core/strings.py
+++ src/main/core/strings.py
@@ -7,3 +7,17 @@ message = 'hello pratyay'
 
 if 'hello' in message:
     print('Greeting sent')
+
+name = 'Pratyay'
+name = name.replace('P', 'r')
+print(name)
+
+message = ' This message has a lot of blank spaces  '
+print(message)
+message = message.strip()
+print(message)
+
+
+age = 13
+statement = 'She said her name was {2}, her age was {0} and her father was {1}.'.format(age, 'India', 'Pakistan')
+print(statement)
