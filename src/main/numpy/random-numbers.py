b6b2665147310d1ec5a54386379a9486e16d6f1a
Pratyay Ganguli <pratyay.ganguli@ziroh.com>
2022-11-03 22:53:44 +0530

    added tasks

--- src/main/numpy/random-numbers.py
+++ src/main/numpy/random-numbers.py
@@ -7,3 +7,10 @@ from numpy import random as rd
 smokeCount = rd.randint(2, 15, size=31)
 print(smokeCount)
 
+
+# Todo: Create a function to generate random numbers based on a range
+
+# Todo: Create a function to generate random decimal numbers.
+
+# Todo: Create a function to generate random alphabets in a range
+
