38a674f365111f3026784b8f62f4c5a1bf433807
Pratyay Ganguli <pratyay.ganguli@ziroh.com>
2022-11-03 22:31:21 +0530

    Added data in numpy package

---
+++ src/main/numpy/infants.py
@@ -0,0 +1,20 @@
+'''
+    @author Pratyay Ganguli
+'''
+
+import numpy as np
+import random as rd
+
+# Data will contain the weight and gender of the infants.
+
+data = np.array([
+    [2.3, 13],
+    [2.4, 10],
+    [3.1, 9],
+    [1.3, 2],
+    [3.8, 4],
+    [4.4, 10],
+    [2.5, 5]
+])
+
+print(data)
