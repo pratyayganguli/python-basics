2e78d638650654553c53faaad0d693d242746724
Pratyay Ganguli <pratyay.ganguli@ziroh.com>
2022-11-05 12:59:24 +0530

    Added numpy scripts

--- src/main/numpy/random-numbers.py
+++ src/main/numpy/random-numbers.py
@@ -2,15 +2,22 @@
     @author Pratyay Ganguli
 '''
 
-from numpy import random as rd
-
-smokeCount = rd.randint(2, 15, size=31)
-print(smokeCount)
-
+import numpy as np
 
 # Todo: Create a function to generate random numbers based on a range
 
+def generateRandIntArray(lowerLimit, upperLimit, size):
+    return np.random.randint(lowerLimit, upperLimit, size, dtype=int)
+
 # Todo: Create a function to generate random decimal numbers.
+
+def generateRandDecimalArray():
+    return np.random.random(size=(3, 5))
+
+
 
 # Todo: Create a function to generate random alphabets in a range
 
+
+print(generateRandIntArray(2, 10, 6))
+print(generateRandDecimalArray())
